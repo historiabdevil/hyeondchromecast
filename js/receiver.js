bb76c228aa841457cd2af5bb3bb876b6cf9d2dfc
hyeond <blind3256@!>
2021-08-11 03:26:51 +0900

    test

--- js/receiver.js
+++ js/receiver.js
@@ -2,7 +2,7 @@ const LOG_RECEIVER_TAG = "RECV_CHROME";
 const context = cast.framework.CastReceiverContext.getInstance();
 const playerManager = context.getPlayerManager();
 const playbackConfig = new cast.framework.PlaybackConfig();
-
+cast.framework.CastReceiverContext.getInstance().setLoggerLevel(cast.framework.LoggerLevel.DEBUG);
 playerManager.addEventListener(
     cast.framework.events.EventType.ERROR, (event) => {
         if (event && event.detailedErrorCode == 905) {
@@ -12,6 +12,7 @@ playerManager.addEventListener(
 playerManager.setMessageInterceptor(
     cast.framework.messages.MessageType.LOAD, loadRequestData => {
         console.log(loadRequestData);
+        playbackConfig.licenUrl = loadRequestData.media.customData['license_url'];
         return loadRequestData;
     }
 );
