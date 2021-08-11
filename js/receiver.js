7800661a470f9d8ea3d26d144aec17c663f11238
hyeond <blind3256@!>
2021-08-11 23:08:20 +0900

    test

--- js/receiver.js
+++ js/receiver.js
@@ -21,15 +21,15 @@ limitations under the License.
 
 'use strict';
 
-import { CastQueue } from './queuing.js';
-import { AdsTracker, SenderTracker, ContentTracker } from './cast_analytics.js';
+import {CastQueue} from './queuing.js';
+import {AdsTracker, SenderTracker, ContentTracker} from './cast_analytics.js';
 
 /**
  * Constants to be used for fetching media by entity from sample repository.
  */
 const ID_REGEX = '\/?([^\/]+)\/?$';
-const CONTENT_URL = 
-  'https://storage.googleapis.com/cpe-sample-media/content.json';
+const CONTENT_URL =
+    'https://storage.googleapis.com/cpe-sample-media/content.json';
 
 const context = cast.framework.CastReceiverContext.getInstance();
 const playerManager = context.getPlayerManager();
@@ -59,14 +59,14 @@ castDebugLogger.showDebugLogs(true);
  * Set verbosity level for Core events.
  */
 castDebugLogger.loggerLevelByEvents = {
-  'cast.framework.events.category.CORE':
+    'cast.framework.events.category.CORE':
     cast.framework.LoggerLevel.INFO,
-  'cast.framework.events.EventType.MEDIA_STATUS':
+    'cast.framework.events.EventType.MEDIA_STATUS':
     cast.framework.LoggerLevel.DEBUG
 };
 
 if (!castDebugLogger.loggerLevelByTags) {
-  castDebugLogger.loggerLevelByTags = {};
+    castDebugLogger.loggerLevelByTags = {};
 }
 
 /**
@@ -74,21 +74,21 @@ if (!castDebugLogger.loggerLevelByTags) {
  * Enables log messages for error, warn, info and debug.
  */
 castDebugLogger.loggerLevelByTags[LOG_RECEIVER_TAG] =
-  cast.framework.LoggerLevel.DEBUG;
+    cast.framework.LoggerLevel.DEBUG;
 
 /**
  * Example of how to listen for events on playerManager.
  */
 playerManager.addEventListener(
-  cast.framework.events.EventType.ERROR, (event) => {
-    castDebugLogger.error(LOG_RECEIVER_TAG,
-      'Detailed Error Code - ' + event.detailedErrorCode);
-    if (event && event.detailedErrorCode == 905) {
-      castDebugLogger.error(LOG_RECEIVER_TAG,
-        'LOAD_FAILED: Verify the load request is set up ' +
-        'properly and the media is able to play.');
-    }
-});
+    cast.framework.events.EventType.ERROR, (event) => {
+        castDebugLogger.error(LOG_RECEIVER_TAG,
+            'Detailed Error Code - ' + event.detailedErrorCode);
+        if (event && event.detailedErrorCode == 905) {
+            castDebugLogger.error(LOG_RECEIVER_TAG,
+                'LOAD_FAILED: Verify the load request is set up ' +
+                'properly and the media is able to play.');
+        }
+    });
 
 /**
  * Example analytics tracking implementation. See cast_analytics.js. Must
@@ -107,28 +107,28 @@ const contentTracker = new ContentTracker();
  * mediainformation. Usually obtained through a load interceptor.
  */
 function addBreaks(mediaInformation) {
-  castDebugLogger.debug(LOG_RECEIVER_TAG, "addBreaks: " +
-    JSON.stringify(mediaInformation));
-  return fetchMediaById('fbb_ad')
-  .then((clip1) => {
-    mediaInformation.breakClips = [
-      {
-        id: 'fbb_ad',
-        title: clip1.title,
-        contentUrl: clip1.stream.dash,
-        contentType: 'application/dash+xml',
-        whenSkippable: 5
-      }
-    ];
+    castDebugLogger.debug(LOG_RECEIVER_TAG, "addBreaks: " +
+        JSON.stringify(mediaInformation));
+    return fetchMediaById('fbb_ad')
+        .then((clip1) => {
+            mediaInformation.breakClips = [
+                {
+                    id: 'fbb_ad',
+                    title: clip1.title,
+                    contentUrl: clip1.stream.dash,
+                    contentType: 'application/dash+xml',
+                    whenSkippable: 5
+                }
+            ];
 
-    mediaInformation.breaks = [
-      {
-        id: 'pre-roll',
-        breakClipIds: ['fbb_ad'],
-        position: 0
-      }
-    ];
-  });
+            mediaInformation.breaks = [
+                {
+                    id: 'pre-roll',
+                    breakClipIds: ['fbb_ad'],
+                    position: 0
+                }
+            ];
+        });
 }
 
 /**
@@ -138,25 +138,23 @@ function addBreaks(mediaInformation) {
  * @return {Promise} Contains the media information of the desired entity.
  */
 function fetchMediaById(id) {
-  castDebugLogger.debug(LOG_RECEIVER_TAG, "fetching id: " + id);
+    castDebugLogger.debug(LOG_RECEIVER_TAG, "fetching id: " + id);
 
-  return new Promise((accept, reject) => {
-    fetch(CONTENT_URL)
-    .then((response) => response.json())
-    .then((obj) => {
-      if (obj) {
-        if (obj[id]) {
-          accept(obj[id]);
-        }
-        else {
-          reject(`${id} not found in repository`);
-        }
-      }
-      else {
-        reject('Content repository not found.');
-      }
+    return new Promise((accept, reject) => {
+        fetch(CONTENT_URL)
+            .then((response) => response.json())
+            .then((obj) => {
+                if (obj) {
+                    if (obj[id]) {
+                        accept(obj[id]);
+                    } else {
+                        reject(`${id} not found in repository`);
+                    }
+                } else {
+                    reject('Content repository not found.');
+                }
+            });
     });
-  });
 }
 
 
@@ -164,66 +162,85 @@ function fetchMediaById(id) {
  * Intercept the LOAD request to load and set the contentUrl and add ads.
  */
 playerManager.setMessageInterceptor(
-  cast.framework.messages.MessageType.LOAD, loadRequestData => {
-    castDebugLogger.debug(LOG_RECEIVER_TAG,
-      `loadRequestData: ${JSON.stringify(loadRequestData)}`);
-    
-    // If the loadRequestData is incomplete return an error message
-    if (!loadRequestData || !loadRequestData.media) {
-      const error = new cast.framework.messages.ErrorData(
-        cast.framework.messages.ErrorType.LOAD_FAILED);
-      error.reason = cast.framework.messages.ErrorReason.INVALID_REQUEST;
-      return error;
-    }
-
-    // check all content source fields for asset URL or ID
-    let source = loadRequestData.media.contentUrl
-      || loadRequestData.media.entity || loadRequestData.media.contentId;
-
-    // If there is no source or a malformed ID then return an error.
-    if (!source || source == "" || !source.match(ID_REGEX)) {
-      let error = new cast.framework.messages.ErrorData(
-        cast.framework.messages.ErrorType.LOAD_FAILED);
-      error.reason = cast.framework.messages.ErrorReason.INVALID_REQUEST;
-      return error;
-    }
-
-    let sourceId = source.match(ID_REGEX)[1];
-
-    // Add breaks to the media information and set the contentUrl
-    return addBreaks(loadRequestData.media)
-    .then(() => {
-      // If the source is a url that points to an asset don't fetch from backend
-      if (sourceId.includes('.')) {
+    cast.framework.messages.MessageType.LOAD, loadRequestData => {
         castDebugLogger.debug(LOG_RECEIVER_TAG,
-          "Interceptor received full URL");
-        loadRequestData.media.contentUrl = source;
-        return loadRequestData;
-      }
+            `loadRequestData: ${JSON.stringify(loadRequestData)}`);
 
-      // Fetch the contentUrl if provided an ID or entity URL
-      else {
-        castDebugLogger.debug(LOG_RECEIVER_TAG, "Interceptor received ID");
-        return fetchMediaById(sourceId)
-        .then((item) => {
-          let metadata = new cast.framework.messages.GenericMediaMetadata();
-          metadata.title = item.title;
-          metadata.subtitle = item.description;
-          loadRequestData.media.contentId = item.stream.dash;
-          loadRequestData.media.contentType = 'application/dash+xml';
-          loadRequestData.media.metadata = metadata;
-          return loadRequestData;
-        })
-      }
-    })
-    .catch((errorMessage) => {
-      let error = new cast.framework.messages.ErrorData(
-        cast.framework.messages.ErrorType.LOAD_FAILED);
-      error.reason = cast.framework.messages.ErrorReason.INVALID_REQUEST;
-      castDebugLogger.error(LOG_RECEIVER_TAG, errorMessage);
-      return error;
-    });
-  }
+        // If the loadRequestData is incomplete return an error message
+        if (!loadRequestData || !loadRequestData.media) {
+            const error = new cast.framework.messages.ErrorData(
+                cast.framework.messages.ErrorType.LOAD_FAILED);
+            error.reason = cast.framework.messages.ErrorReason.INVALID_REQUEST;
+            return error;
+        }
+
+        // check all content source fields for asset URL or ID
+        let source = loadRequestData.media.contentUrl
+            || loadRequestData.media.entity || loadRequestData.media.contentId;
+
+        // If there is no source or a malformed ID then return an error.
+        if (!source || source == "" || !source.match(ID_REGEX)) {
+            let error = new cast.framework.messages.ErrorData(
+                cast.framework.messages.ErrorType.LOAD_FAILED);
+            error.reason = cast.framework.messages.ErrorReason.INVALID_REQUEST;
+            return error;
+        }
+
+        let sourceId = source.match(ID_REGEX)[1];
+
+        // Add breaks to the media information and set the contentUrl
+        return addBreaks(loadRequestData.media)
+            .then(() => {
+                // If the source is a url that points to an asset don't fetch from backend
+                if (sourceId.includes('.')) {
+                    castDebugLogger.debug(LOG_RECEIVER_TAG,
+                        "Interceptor received full URL");
+                    loadRequestData.media.contentUrl = source;
+                    if (request.media.customData.licenseUrl) {
+                        playerManager.setMediaPlaybackInfoHandler((loadRequest, playbackConfig) => {
+                            playbackConfig.protectionSystem = cast.framework.ContentProtection.WIDEVINE;
+                            playbackConfig.licenseUrl = loadRequestData.media.customData.licenseUrl;
+                            if (typeof loadRequestData.media.customData.credentials === 'boolean' || typeof loadRequestData.media.customData.licenseHeaders === 'object') {
+                                playbackConfig.licenseRequestHandler = requestInfo => {
+                                    if (typeof loadRequestData.media.customData.credentials === 'boolean') {
+                                        requestInfo.withCredentials = request.media.customData.credentials;
+                                    }
+                                    if (typeof loadRequestData.media.customData.licenseHeaders === 'object') {
+                                        requestInfo.headers = request.media.customData.licenseHeaders;
+                                    }
+                                };
+                            }
+                            return playbackConfig;
+                        });
+                    }
+
+
+                    return loadRequestData;
+                }
+
+                // Fetch the contentUrl if provided an ID or entity URL
+                else {
+                    castDebugLogger.debug(LOG_RECEIVER_TAG, "Interceptor received ID");
+                    return fetchMediaById(sourceId)
+                        .then((item) => {
+                            let metadata = new cast.framework.messages.GenericMediaMetadata();
+                            metadata.title = item.title;
+                            metadata.subtitle = item.description;
+                            loadRequestData.media.contentId = item.stream.dash;
+                            loadRequestData.media.contentType = 'application/dash+xml';
+                            loadRequestData.media.metadata = metadata;
+                            return loadRequestData;
+                        })
+                }
+            })
+            .catch((errorMessage) => {
+                let error = new cast.framework.messages.ErrorData(
+                    cast.framework.messages.ErrorType.LOAD_FAILED);
+                error.reason = cast.framework.messages.ErrorReason.INVALID_REQUEST;
+                castDebugLogger.error(LOG_RECEIVER_TAG, errorMessage);
+                return error;
+            });
+    }
 );
 
 const playbackConfig = new cast.framework.PlaybackConfig();
@@ -234,7 +251,7 @@ const playbackConfig = new cast.framework.PlaybackConfig();
  */
 playbackConfig.autoResumeDuration = 5;
 castDebugLogger.info(LOG_RECEIVER_TAG,
-  `autoResumeDuration set to: ${playbackConfig.autoResumeDuration}`);
+    `autoResumeDuration set to: ${playbackConfig.autoResumeDuration}`);
 
 /**
  * Set the control buttons in the UI controls.
@@ -246,39 +263,39 @@ controls.clearDefaultSlotAssignments();
  * Assign buttons to control slots.
  */
 controls.assignButton(
-  cast.framework.ui.ControlsSlot.SLOT_SECONDARY_1,
-  cast.framework.ui.ControlsButton.QUEUE_PREV
+    cast.framework.ui.ControlsSlot.SLOT_SECONDARY_1,
+    cast.framework.ui.ControlsButton.QUEUE_PREV
 );
 controls.assignButton(
-  cast.framework.ui.ControlsSlot.SLOT_PRIMARY_1,
-  cast.framework.ui.ControlsButton.CAPTIONS
+    cast.framework.ui.ControlsSlot.SLOT_PRIMARY_1,
+    cast.framework.ui.ControlsButton.CAPTIONS
 );
 controls.assignButton(
-  cast.framework.ui.ControlsSlot.SLOT_PRIMARY_2,
-  cast.framework.ui.ControlsButton.SEEK_FORWARD_15
+    cast.framework.ui.ControlsSlot.SLOT_PRIMARY_2,
+    cast.framework.ui.ControlsButton.SEEK_FORWARD_15
 );
 controls.assignButton(
-  cast.framework.ui.ControlsSlot.SLOT_SECONDARY_2,
-  cast.framework.ui.ControlsButton.QUEUE_NEXT
+    cast.framework.ui.ControlsSlot.SLOT_SECONDARY_2,
+    cast.framework.ui.ControlsButton.QUEUE_NEXT
 );
 playbackConfig.licenseUrl = 'https://license.pallycon.com/ri/licenseManager.do';
 playbackConfig.protectionSystem = cast.framework.ContentProtection.WIDEVINE;
 playbackConfig.licenseRequestHandler = requestInfo => {
-  requestInfo.withCredentials = true;
+    requestInfo.withCredentials = true;
 };
 
 context.start({
-  queue: new CastQueue(),
-  playbackConfig: playbackConfig,
-  supportedCommands: cast.framework.messages.Command.ALL_BASIC_MEDIA |
-                      cast.framework.messages.Command.QUEUE_PREV |
-                      cast.framework.messages.Command.QUEUE_NEXT |
-                      cast.framework.messages.Command.STREAM_TRANSFER
+    queue: new CastQueue(),
+    playbackConfig: playbackConfig,
+    supportedCommands: cast.framework.messages.Command.ALL_BASIC_MEDIA |
+        cast.framework.messages.Command.QUEUE_PREV |
+        cast.framework.messages.Command.QUEUE_NEXT |
+        cast.framework.messages.Command.STREAM_TRANSFER
 });
 
 context.getPlayerManager().setMediaPlaybackInfoHandler((loadRequest, playbackConfig) => {
-  if (loadRequest.media.customData && loadRequest.media.customData.licenseUrl) {
-    playbackConfig.licenseUrl = loadRequest.media.customData.licenseUrl;
-  }
-  return playbackConfig;
+    if (loadRequest.media.customData && loadRequest.media.customData.licenseUrl) {
+        playbackConfig.licenseUrl = loadRequest.media.customData.licenseUrl;
+    }
+    return playbackConfig;
 });
