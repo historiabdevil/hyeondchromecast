960f76e25888da6bd21865ff84204f1485f81501
hyeond <blind3256@!>
2021-08-11 09:24:38 +0900

    test

--- js/receiver.js
+++ js/receiver.js
@@ -1,5 +1,6 @@
 const LOG_RECEIVER_TAG = "RECV_CHROME";
 const context = cast.framework.CastReceiverContext.getInstance();
+const castDebugLogger = cast.debug.CastDebugLogger.getInstance();
 const playerManager = context.getPlayerManager();
 const playbackConfig = new cast.framework.PlaybackConfig();
 cast.framework.CastReceiverContext.getInstance().setLoggerLevel(cast.framework.LoggerLevel.DEBUG);
@@ -12,17 +13,45 @@ playerManager.addEventListener(
 playerManager.setMessageInterceptor(
     cast.framework.messages.MessageType.LOAD, loadRequestData => {
         console.log(loadRequestData);
+        playbackConfig.licenUrl = loadRequestData.media.customData['license_url'];
+        playbackConfig.protectionSystem = cast.framework.ContentProtection.WIDEVINE;
 
         return loadRequestData;
     }
 );
 
-playerManager.setMediaPlaybackInfoHandler((loadRequestData, playbackConfig) =>{
+playerManager.setMediaPlaybackInfoHandler((loadRequestData, playbackConfig) => {
     playbackConfig.licenUrl = loadRequestData.media.customData['license_url'];
     playbackConfig.protectionSystem = cast.framework.ContentProtection.WIDEVINE;
 })
 
+const controls = cast.framework.ui.Controls.getInstance();
+controls.clearDefaultSlotAssignments();
+
+/**
+ * Assign buttons to control slots.
+ */
+controls.assignButton(
+    cast.framework.ui.ControlsSlot.SLOT_SECONDARY_1,
+    cast.framework.ui.ControlsButton.QUEUE_PREV
+);
+controls.assignButton(
+    cast.framework.ui.ControlsSlot.SLOT_PRIMARY_1,
+    cast.framework.ui.ControlsButton.CAPTIONS
+);
+controls.assignButton(
+    cast.framework.ui.ControlsSlot.SLOT_PRIMARY_2,
+    cast.framework.ui.ControlsButton.SEEK_FORWARD_15
+);
+controls.assignButton(
+    cast.framework.ui.ControlsSlot.SLOT_SECONDARY_2,
+    cast.framework.ui.ControlsButton.QUEUE_NEXT
+);
+
 context.start({
-    playerManager: playerManager,
-    playbackConfig: playbackConfig
+    playbackConfig: playbackConfig,
+    supportedCommands: cast.framework.messages.Command.ALL_BASIC_MEDIA |
+        cast.framework.messages.Command.QUEUE_PREV |
+        cast.framework.messages.Command.QUEUE_NEXT |
+        cast.framework.messages.Command.STREAM_TRANSFER
 });
