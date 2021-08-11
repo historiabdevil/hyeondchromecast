63c07d283fff9bd77977a77fb096234ebed450be
hyeond <blind3256@!>
2021-08-11 23:17:27 +0900

    test

--- js/receiver.js
+++ js/receiver.js
@@ -1,289 +1,109 @@
-/*
-Copyright 2020 Google LLC. All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 /**
- * This sample demonstrates how to build your own Receiver for use with Google
- * Cast.
+ @license Copyright (c) 2018-2020 Radiant Media Player | https://www.radiantmediaplayer.com
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
  */
-
-'use strict';
-
-import {CastQueue} from './queuing.js';
-import {AdsTracker, SenderTracker, ContentTracker} from './cast_analytics.js';
-
-/**
- * Constants to be used for fetching media by entity from sample repository.
- */
-const ID_REGEX = '\/?([^\/]+)\/?$';
-const CONTENT_URL =
-    'https://storage.googleapis.com/cpe-sample-media/content.json';
 
 const context = cast.framework.CastReceiverContext.getInstance();
 const playerManager = context.getPlayerManager();
+const options = new cast.framework.CastReceiverOptions();
+options.customNamespaces = {};
+options.customNamespaces['urn:x-cast:com.radiantmediaplayer.cast'] = cast.framework.system.MessageType.JSON;
 
-
-const LOG_RECEIVER_TAG = 'Receiver';
-
-/**
- * Debug Logger
- */
-const castDebugLogger = cast.debug.CastDebugLogger.getInstance();
-
-/**
- * WARNING: Make sure to turn off debug logger for production release as it
- * may expose details of your app.
- * Uncomment below line to enable debug logger and show a 'DEBUG MODE' tag at
- * top left corner.
- */
-castDebugLogger.setEnabled(true);
-
-/**
- * Uncomment below line to show debug overlay.
- */
-castDebugLogger.showDebugLogs(true);
-
-/**
- * Set verbosity level for Core events.
- */
-castDebugLogger.loggerLevelByEvents = {
-    'cast.framework.events.category.CORE':
-    cast.framework.LoggerLevel.INFO,
-    'cast.framework.events.EventType.MEDIA_STATUS':
-    cast.framework.LoggerLevel.DEBUG
-};
-
-if (!castDebugLogger.loggerLevelByTags) {
-    castDebugLogger.loggerLevelByTags = {};
+// handle ad breaks
+function addBreakToMedia(media, adTagUrl, currentTime) {
+    media.breakClips = [{
+        id: 'bc1',
+        vastAdsRequest: {
+            adTagUrl: adTagUrl
+        }
+    }];
+    media.breaks = [{
+        id: 'b1',
+        breakClipIds: ['bc1'],
+        position: currentTime
+    }];
 }
 
-/**
- * Set verbosity level for custom tag.
- * Enables log messages for error, warn, info and debug.
- */
-castDebugLogger.loggerLevelByTags[LOG_RECEIVER_TAG] =
-    cast.framework.LoggerLevel.DEBUG;
+function addVmapBreakToMedia(media, vmapAdTagUrl) {
+    media.vmapAdsRequest = {
+        adTagUrl: vmapAdTagUrl
+    };
+}
 
-/**
- * Example of how to listen for events on playerManager.
- */
+// send signal to local player that the current stream has finished
 playerManager.addEventListener(
-    cast.framework.events.EventType.ERROR, (event) => {
-        castDebugLogger.error(LOG_RECEIVER_TAG,
-            'Detailed Error Code - ' + event.detailedErrorCode);
-        if (event && event.detailedErrorCode == 905) {
-            castDebugLogger.error(LOG_RECEIVER_TAG,
-                'LOAD_FAILED: Verify the load request is set up ' +
-                'properly and the media is able to play.');
-        }
-    });
-
-/**
- * Example analytics tracking implementation. See cast_analytics.js. Must
- * complete TODO item in google_analytics.js.
- */
-const adTracker = new AdsTracker();
-const senderTracker = new SenderTracker();
-const contentTracker = new ContentTracker();
-// adTracker.startTracking();
-// senderTracker.startTracking();
-// contentTracker.startTracking();
-
-/**
- * Adds an ad to the beginning of the desired content.
- * @param {cast.framework.messages.MediaInformation} mediaInformation The target
- * mediainformation. Usually obtained through a load interceptor.
- */
-function addBreaks(mediaInformation) {
-    castDebugLogger.debug(LOG_RECEIVER_TAG, "addBreaks: " +
-        JSON.stringify(mediaInformation));
-    return fetchMediaById('fbb_ad')
-        .then((clip1) => {
-            mediaInformation.breakClips = [
-                {
-                    id: 'fbb_ad',
-                    title: clip1.title,
-                    contentUrl: clip1.stream.dash,
-                    contentType: 'application/dash+xml',
-                    whenSkippable: 5
-                }
-            ];
-
-            mediaInformation.breaks = [
-                {
-                    id: 'pre-roll',
-                    breakClipIds: ['fbb_ad'],
-                    position: 0
-                }
-            ];
-        });
-}
-
-/**
- * Obtains media from a remote repository.
- * @param  {Number} Entity or ID that contains a key to media in JSON hosted
- * by CONTENT_URL.
- * @return {Promise} Contains the media information of the desired entity.
- */
-function fetchMediaById(id) {
-    castDebugLogger.debug(LOG_RECEIVER_TAG, "fetching id: " + id);
-
-    return new Promise((accept, reject) => {
-        fetch(CONTENT_URL)
-            .then((response) => response.json())
-            .then((obj) => {
-                if (obj) {
-                    if (obj[id]) {
-                        accept(obj[id]);
-                    } else {
-                        reject(`${id} not found in repository`);
-                    }
-                } else {
-                    reject('Content repository not found.');
-                }
+    cast.framework.events.EventType.MEDIA_FINISHED,
+    event => {
+        if (event && event.endedReason === 'END_OF_STREAM') {
+            context.sendCustomMessage('urn:x-cast:com.radiantmediaplayer.cast', undefined, {
+                type: 'STATUS',
+                message: 'MEDIA_FINISHED'
             });
-    });
-}
-
-
-/**
- * Intercept the LOAD request to load and set the contentUrl and add ads.
- */
-playerManager.setMessageInterceptor(
-    cast.framework.messages.MessageType.LOAD, loadRequestData => {
-        castDebugLogger.debug(LOG_RECEIVER_TAG,
-            `loadRequestData: ${JSON.stringify(loadRequestData)}`);
-
-        // If the loadRequestData is incomplete return an error message
-        if (!loadRequestData || !loadRequestData.media) {
-            const error = new cast.framework.messages.ErrorData(
-                cast.framework.messages.ErrorType.LOAD_FAILED);
-            error.reason = cast.framework.messages.ErrorReason.INVALID_REQUEST;
-            return error;
         }
-
-        // check all content source fields for asset URL or ID
-        let source = loadRequestData.media.contentUrl
-            || loadRequestData.media.entity || loadRequestData.media.contentId;
-
-        // If there is no source or a malformed ID then return an error.
-        if (!source || source == "" || !source.match(ID_REGEX)) {
-            let error = new cast.framework.messages.ErrorData(
-                cast.framework.messages.ErrorType.LOAD_FAILED);
-            error.reason = cast.framework.messages.ErrorReason.INVALID_REQUEST;
-            return error;
-        }
-
-        let sourceId = source.match(ID_REGEX)[1];
-
-        // Add breaks to the media information and set the contentUrl
-        return addBreaks(loadRequestData.media)
-            .then(() => {
-                // If the source is a url that points to an asset don't fetch from backend
-                if (sourceId.includes('.')) {
-                    castDebugLogger.debug(LOG_RECEIVER_TAG,
-                        "Interceptor received full URL");
-                    loadRequestData.media.contentUrl = source;
-                    if (request.media.customData.licenseUrl) {
-                        playerManager.setMediaPlaybackInfoHandler((loadRequest, playbackConfig) => {
-                            playbackConfig.protectionSystem = cast.framework.ContentProtection.WIDEVINE;
-                            playbackConfig.licenseUrl = loadRequestData.media.customData.licenseUrl;
-                            if (typeof loadRequestData.media.customData.credentials === 'boolean' || typeof loadRequestData.media.customData.licenseHeaders === 'object') {
-                                playbackConfig.licenseRequestHandler = requestInfo => {
-                                    if (typeof loadRequestData.media.customData.credentials === 'boolean') {
-                                        requestInfo.withCredentials = request.media.customData.credentials;
-                                    }
-                                    if (typeof loadRequestData.media.customData.licenseHeaders === 'object') {
-                                        requestInfo.headers = request.media.customData.licenseHeaders;
-                                    }
-                                };
-                            }
-                            return playbackConfig;
-                        });
-                    }
-
-
-                    return loadRequestData;
-                }
-
-                // Fetch the contentUrl if provided an ID or entity URL
-                else {
-                    castDebugLogger.debug(LOG_RECEIVER_TAG, "Interceptor received ID");
-                    return fetchMediaById(sourceId)
-                        .then((item) => {
-                            let metadata = new cast.framework.messages.GenericMediaMetadata();
-                            metadata.title = item.title;
-                            metadata.subtitle = item.description;
-                            loadRequestData.media.contentId = item.stream.dash;
-                            loadRequestData.media.contentType = 'application/dash+xml';
-                            loadRequestData.media.metadata = metadata;
-                            return loadRequestData;
-                        })
-                }
-            })
-            .catch((errorMessage) => {
-                let error = new cast.framework.messages.ErrorData(
-                    cast.framework.messages.ErrorType.LOAD_FAILED);
-                error.reason = cast.framework.messages.ErrorReason.INVALID_REQUEST;
-                castDebugLogger.error(LOG_RECEIVER_TAG, errorMessage);
-                return error;
-            });
     }
 );
 
-const playbackConfig = new cast.framework.PlaybackConfig();
-
-/**
- * Set the player to start playback as soon as there are five seconds of
- * media content buffered. Default is 10.
- */
-playbackConfig.autoResumeDuration = 5;
-castDebugLogger.info(LOG_RECEIVER_TAG,
-    `autoResumeDuration set to: ${playbackConfig.autoResumeDuration}`);
-
-/**
- * Set the control buttons in the UI controls.
- */
-const controls = cast.framework.ui.Controls.getInstance();
-controls.clearDefaultSlotAssignments();
-
-/**
- * Assign buttons to control slots.
- */
-controls.assignButton(
-    cast.framework.ui.ControlsSlot.SLOT_SECONDARY_1,
-    cast.framework.ui.ControlsButton.QUEUE_PREV
+// on LOAD adjust behaviour of CAF receiver
+playerManager.setMessageInterceptor(
+    cast.framework.messages.MessageType.LOAD,
+    request => {
+        if (request.media && request.media.customData) {
+            if (request.media.customData.adTagUrl) {
+                if (request.media.customData.currentTime) {
+                    // VOD and currentTime is not 0
+                    addBreakToMedia(request.media, request.media.customData.adTagUrl, Math.floor(request.media.customData.currentTime / 1000));
+                } else {
+                    // Live or DVR or VOD with currentTime === 0
+                    addBreakToMedia(request.media, request.media.customData.adTagUrl, 0);
+                }
+            } else if (request.media.customData.vmapAdsRequest) {
+                // VMAP requests
+                addVmapBreakToMedia(request.media, request.media.customData.vmapAdsRequest);
+            }
+            // Live UI on CAF receiver
+            if (request.media.customData.isLive) {
+                request.media.streamType = cast.framework.messages.StreamType.LIVE;
+                // for live stream - disable seek as we do not support DVR stream on Google Cast yet
+                playerManager.removeSupportedMediaCommands(cast.framework.messages.Command.SEEK, true);
+                playerManager.setMessageInterceptor(
+                    cast.framework.messages.MessageType.SEEK,
+                    seekData => {
+                        // if the SEEK supported media command is disabled, block seeking
+                        if (!(playerManager.getSupportedMediaCommands() && cast.framework.messages.Command.SEEK)) {
+                            return null;
+                        }
+                        return seekData;
+                    });
+            }
+            // DRM
+            if (request.media.customData.licenseUrl) {
+                playerManager.setMediaPlaybackInfoHandler((loadRequest, playbackConfig) => {
+                    playbackConfig.protectionSystem = cast.framework.ContentProtection.WIDEVINE;
+                    playbackConfig.licenseUrl = request.media.customData.licenseUrl;
+                    if (typeof request.media.customData.credentials === 'boolean' || typeof request.media.customData.licenseHeaders === 'object') {
+                        playbackConfig.licenseRequestHandler = requestInfo => {
+                            if (typeof request.media.customData.credentials === 'boolean') {
+                                requestInfo.withCredentials = request.media.customData.credentials;
+                            }
+                            if (typeof request.media.customData.licenseHeaders === 'object') {
+                                requestInfo.headers = request.media.customData.licenseHeaders;
+                            }
+                        };
+                    }
+                    return playbackConfig;
+                });
+            }
+        }
+        return request;
+    }
 );
-controls.assignButton(
-    cast.framework.ui.ControlsSlot.SLOT_PRIMARY_1,
-    cast.framework.ui.ControlsButton.CAPTIONS
-);
-controls.assignButton(
-    cast.framework.ui.ControlsSlot.SLOT_PRIMARY_2,
-    cast.framework.ui.ControlsButton.SEEK_FORWARD_15
-);
-controls.assignButton(
-    cast.framework.ui.ControlsSlot.SLOT_SECONDARY_2,
-    cast.framework.ui.ControlsButton.QUEUE_NEXT
-);
 
-context.start({
-    queue: new CastQueue(),
-    playbackConfig: playbackConfig,
-    supportedCommands: cast.framework.messages.Command.ALL_BASIC_MEDIA |
-        cast.framework.messages.Command.QUEUE_PREV |
-        cast.framework.messages.Command.QUEUE_NEXT |
-        cast.framework.messages.Command.STREAM_TRANSFER
-});
+context.start(options);
