728eab2c5557e5e6416e6e0c7bff3361e5fff338
hyeond <blind3256@!>
2021-08-11 16:24:55 +0900

    test

--- js/receiver.js
+++ js/receiver.js
@@ -15,7 +15,7 @@ window.onload = function () {
     playbackConfig.licenseUrl = 'https://license-global.pallycon.com/ri/licenseManager.do';
     playbackConfig.protectionSystem = cast.framework.ContentProtection.WIDEVINE;
     playbackConfig.licenseRequestHandler = requestInfo => {
-        requestInfo.withCredentials = true;
+        // requestInfo.withCredentials = true;
         requestInfo.headers = {
             'pallycon-customdata-v2': 'eyJkcm1fdHlwZSI6IldpZGV2aW5lIiwic2l0ZV9pZCI6IkpLUEIiLCJ1c2VyX2lkIjoiY2F0ZW5vaWR0ZXN0IiwiY2lkIjoiMjAxOTA5MDktZXYwMDl5dXEiLCJ0b2tlbiI6ImZ3Zk1SODAxU0RsNEhkMDBDZGtLMkJrWUQ0cTBlSmJDU2w4emZBQXRZTHJVM2ZzaXVtbmNtcm0wS051OWVxSXpRTlhldkgyck1Mb2YzT3MySmxsRldUZFA2NWl5d2lMZ0VWakQwVHllVGx3M2N1ODNsbVhhTVVONERiaEtGWURGSXhhK2grXC9vSjNsTkVjb0hOcTF2UGVJZ1prYm1cL0JWZDNhQ0lERDZxdDY3WURzZ1pnaWlKUWFZSzBiOFZKSnlQIiwidGltZXN0YW1wIjoiMjAyMS0wOC0xMVQwNjoyMDo0N1oiLCJoYXNoIjoiK09CUkFQMVlQS1AxMGVsd3lwSUFOXC9XXC9qZ3ZnVkx0NjIxMHZOcEZ0SlhRPSJ9'
         }
@@ -41,7 +41,7 @@ window.onload = function () {
         playbackConfig.licenseUrl = 'https://license-global.pallycon.com/ri/licenseManager.do';
         playbackConfig.protectionSystem = cast.framework.ContentProtection.WIDEVINE;
         playbackConfig.licenseRequestHandler = requestInfo => {
-            requestInfo.withCredentials = true;
+            // requestInfo.withCredentials = true;
             requestInfo.headers = {
                 'pallycon-customdata-v2': 'eyJkcm1fdHlwZSI6IldpZGV2aW5lIiwic2l0ZV9pZCI6IkpLUEIiLCJ1c2VyX2lkIjoiY2F0ZW5vaWR0ZXN0IiwiY2lkIjoiMjAxOTA5MDktZXYwMDl5dXEiLCJ0b2tlbiI6ImZ3Zk1SODAxU0RsNEhkMDBDZGtLMkJrWUQ0cTBlSmJDU2w4emZBQXRZTHJVM2ZzaXVtbmNtcm0wS051OWVxSXpRTlhldkgyck1Mb2YzT3MySmxsRldUZFA2NWl5d2lMZ0VWakQwVHllVGx3M2N1ODNsbVhhTVVONERiaEtGWURGSXhhK2grXC9vSjNsTkVjb0hOcTF2UGVJZ1prYm1cL0JWZDNhQ0lERDZxdDY3WURzZ1pnaWlKUWFZSzBiOFZKSnlQIiwidGltZXN0YW1wIjoiMjAyMS0wOC0xMVQwNjoyMDo0N1oiLCJoYXNoIjoiK09CUkFQMVlQS1AxMGVsd3lwSUFOXC9XXC9qZ3ZnVkx0NjIxMHZOcEZ0SlhRPSJ9'
             }
