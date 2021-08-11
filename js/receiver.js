276d5259c165462cc7b096d66d807f66e9864a53
hyeond <blind3256@!>
2021-08-11 16:48:49 +0900

    test

--- js/receiver.js
+++ js/receiver.js
@@ -1,14 +1,14 @@
 window.onload = function () {
-    if (typeof console  != "undefined")
-        if (typeof console.log != 'undefined')
-            console.olog = console.log;
-        else
-            console.olog = function() {};
-
-    console.log = function(message) {
-        console.olog(message);
-        $('#debugDiv').append('<p>' + message + '</p>');
-    };
+    // if (typeof console  != "undefined")
+    //     if (typeof console.log != 'undefined')
+    //         console.olog = console.log;
+    //     else
+    //         console.olog = function() {};
+    //
+    // console.log = function(message) {
+    //     console.olog(message);
+    //     $('#debugDiv').append('<p>' + message + '</p>');
+    // };
     console.error = console.debug = console.info =  console.log
     cast.framework.CastReceiverContext.getInstance().setLoggerLevel(cast.framework.LoggerLevel.DEBUG);
     const LOG_RECEIVER_TAG = "RECV_CHROME";
